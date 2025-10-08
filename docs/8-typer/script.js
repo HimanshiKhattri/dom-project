482672607846201a1362386e3950c8a3ca818de7
HimanshiKhattri <k.himanshi02@gmail.com>
2025-10-08 15:33:11 +0530

    typing library

---
+++ docs/8-typer/script.js
@@ -0,0 +1,37 @@
+const typedTextSpan = document.querySelector('.typed-text');
+const cursor = document.querySelector('.cursor');
+
+const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(charIndex < words[wordIndex].length) {
+        typedTextSpan.textContent += words[wordIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type , 150);
+    } 
+    else {
+        setTimeout(erase , 1000);
+    }  
+}
+
+function erase() {
+    if(charIndex > 0) {
+        typedTextSpan.textContent = words[wordIndex].substring(0 , charIndex - 1)
+        charIndex--;
+        setTimeout(erase , 100)
+    } else {
+        wordIndex++;
+        if(wordIndex >= words.length) {
+            wordIndex = 0;
+        }
+        setTimeout(type , 300); 
+    }  
+  
+}
+
+document.addEventListener('DOMContentLoaded' , function() {
+    setTimeout(type , 1000);
+});
