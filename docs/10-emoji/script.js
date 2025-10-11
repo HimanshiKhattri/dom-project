ec011cc16685e22ffc798dd73c8ea343ac31c8c6
HimanshiKhattri <k.himanshi02@gmail.com>
2025-10-11 15:48:04 +0530

    added emoji game

---
+++ docs/10-emoji/script.js
@@ -0,0 +1,47 @@
+const btn = document.querySelector('#emoji');
+const emojis = [
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '😀',
+  '🤑',
+  '🤨',
+  '🙂',
+  '😊',
+  '😗',
+  '😛',
+  '😏',
+  '🤥',
+  '😴',
+  '🥺',
+  '😧',
+  '😇',
+  '😳',
+  '🙃',
+  '🥴',
+  '🧐',
+  '🤨',
+  '😒',
+  '🤔',
+  '🤭',
+  '🥰',
+  '🤐',
+  '👀',
+  '🤔',
+  '🤪',
+  '🥲',
+  '😃',
+  '😁',
+  '😬',
+];
+
+
+btn.addEventListener('mouseenter' , () => {
+  emoji();
+});
+
+function emoji() {
+  const randomEmoji = Math.floor(Math.random() * emojis.length);
+  btn.textContent = `${emojis[randomEmoji]}`;
+}
