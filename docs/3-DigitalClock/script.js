8e5156d18a919cc40fbdf38c47471fb6177f4329
HimanshiKhattri <k.himanshi02@gmail.com>
2026-03-09 10:48:32 +0530

    small change in digital clock

--- docs/3-DigitalClock/script.js
+++ docs/3-DigitalClock/script.js
@@ -1,6 +1,14 @@
 const clock = document.getElementById('clock');
+const dateEl = document.getElementById('date');
 
 setInterval(() => {
     const date = new Date();
     clock.innerText = date.toLocaleTimeString();
+   
+    
 } );
+
+const date = new Date();
+dateEl.innerText = date.toDateString();
+
+
