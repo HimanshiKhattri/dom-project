c39e65d8bba23086a26a4b7b01db63e558a5b8b9
HimanshiKhattri <k.himanshi02@gmail.com>
2026-03-25 11:39:06 +0530

    js add in iamge slider and quiz app UI

--- docs/image-slider/script.js
+++ docs/image-slider/script.js
@@ -2,38 +2,61 @@ const slides = document.querySelector('.slides');
 const images = document.querySelectorAll('.slides img');
 const btnPrev = document.querySelector('.prev');
 const btnNxt = document.querySelector('.next');
-const dotsBox = document.querySelector('dots')
+const dotsBox = document.querySelector('.dots')
 
-// console.log(typeof(slides))
-// console.log(typeof(images))
-// console.log(images)
-// console.log(images.length)
 
 let index = 0;
+let dots = [];
+
+
 
 // slideshow function
 function slideShow(i) {
     if(i < 0) {
-        console.log(i)
-        console.log(images.length)
         index = images.length - 1;
         console.log(index);
     } else if (i >= images.length) {
         index = 0;
-        console.log(index)
     } else {
         index = i;
-        console.log(index)
+
     }
 
     slides.style.transform = `translateX(${-index * 100}%)`;
+    updateDots()
 }
 
 // button event
 btnPrev.addEventListener("click" ,() => {
     slideShow(index - 1)
-})
+});
 
 btnNxt.addEventListener("click" ,() => {
     slideShow(index + 1)
-})
+});
+
+
+// creat dots dynamically
+images.forEach(( _ , i) => {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if(i === 0) dot.classList.add('active');
+    dot.addEventListener("click" , () => {
+        slideShow(i);
+    });
+    dotsBox.appendChild(dot);
+    dots.push(dot)
+} );
+
+// update dots
+function updateDots() {
+    dots.forEach((dot) => {
+        dot.classList.remove('active');
+    });
+    dots[index].classList.add('active');
+}
+
+// auto slide 
+setInterval(() => {
+    slideShow(index + 1)
+} , 5000);
---
+++ docs/quizApp/script.js
@@ -0,0 +1,5 @@
+const question = document.getElementById('question');
+const answerBtns = document.getElementById('answer-buttons');
+const startBtn = document.getElementById('start-btn');
+const nextBtn = document.getElementById('next-btn');
+const resultShow = document.getElementById('result')
